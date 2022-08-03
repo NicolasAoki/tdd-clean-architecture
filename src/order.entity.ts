9ef5f990efd12469653f145cb644ba8ec8a66128
nicolas aoki <nick_aoki@hotmail.com>
2022-08-03 01:04:16 -0300

    feat: able to attach cpf to an order

--- src/order.entity.ts
+++ src/order.entity.ts
@@ -1,9 +1,12 @@
+import CPF from "./cpf.entity";
+
 export default class Order {
   constructor(
     public description: string,
     public price: number,
     public quantity: number,
     public discountVoucher?: DiscountVouchers,
+    public cpf?: CPF
   ) {
     if (discountVoucher) {
       this.price = price * (1 - discountVoucher.percentage)
--- test/orders.test.ts
+++ test/orders.test.ts
@@ -1,3 +1,4 @@
+import CPF from "../src/cpf.entity"
 import Order, { DiscountVouchers } from "../src/order.entity"
 
 test('should create one order', () => {
@@ -12,4 +13,10 @@ test('should create one order with discount', () => {
   const order = new Order('coffee table', 100, 100, discountVoucher)
   expect(order.description).toBe('coffee table')
   expect(order.price).toBe(75)
+})
+
+test('should attach an order to a cpf', () => {
+  const cpf = new CPF('409.595.180-02')
+  const order = new Order('coffee table', 100, 100, undefined, cpf)
+  expect(order.cpf?.number).toBe('40959518002')
 })
